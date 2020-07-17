9d82cb7cf1a73aaa25ca3fe6c3ca4dfbf156e4b5
hata <hata@aivick.co.jp>
2020-07-17 10:46:19 +0900

    updated circleci

--- config/database.php
+++ config/database.php
@@ -96,7 +96,7 @@ return [
             'host' => '127.0.0.1',
             'port' => '3306',
             'database' => 'circleci_test',
-            'username' => 'root',
+            'username' => 'circleci',
             'password' => '',
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
